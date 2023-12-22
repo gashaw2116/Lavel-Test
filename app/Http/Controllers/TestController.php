1f33e62009834ca935db543ebb3aad53c71f79fa
gashaw2116 <beluel21duc@gmail.com>
2023-12-22 10:52:13 +0300

    Adding Laravel View

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    
+    function text() {
+        
+        return "welcome to our website";
+        //
+    }
+    
+    
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 
 Route::get('/', function () {
     return view('index');
@@ -26,3 +26,18 @@ Route::get('/cart', function() {
 Route::get('/products', function() {
     return view('products');
 });
+*/
+//Route::get('/post','App\Http\Controllers\PostController@index');
+//Route::get('/post/{id}','App\Http\Controllers\PostController@show');
+//Route::resource('/post','App\Http\Controllers\PostController');
+/*Route::resources(
+    [
+    'posts'=>'\App\Http\Controllers\PostController',
+    'students'=>'\App\Http\Controllers\StudentController'
+    ]
+    );
+    Route::resource('student','\App\Http\Controllers\StudentController',
+    [
+        'except' => ['create','show']
+    ]); */
+    Route::get('/tests/text','App\Http\Controllers\TestController@text');
